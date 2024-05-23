bfd37ab77cbe2a48b88715795e5f943e0a0a7e4b
Yaseen Iqbal <yaseen.iqbal@eritheialabs.io>
2024-05-23 19:20:34 +0500

    feat: Added ai suggestions for task description

---
+++ .venv/app.py
@@ -0,0 +1,109 @@
+from flask import Flask,render_template,request,url_for,redirect,flash, jsonify
+from flask_sqlalchemy import SQLAlchemy
+import datetime as dt
+from transformers import pipeline
+from transformers import AutoTokenizer, MistralForCausalLM
+
+
+#initialize the SQLalchemy object
+db = SQLAlchemy()
+
+# name of the database
+db_name = "todo.db" 
+
+#initializing the app
+app = Flask(__name__)
+
+app.secret_key = "arslanwaqar421"
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + db_name
+
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
+
+db.init_app(app)    
+
+class Task(db.Model):
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)  # Added autoincrement=True
+    title = db.Column(db.String(100), nullable=False)
+    description = db.Column(db.Text)
+    Ending_Timestamp = db.Column(db.DateTime)
+    isComplete= db.Column(db.Boolean, default=False)
+    Creation_Timestamp = db.Column(db.DateTime)
+
+
+
+    def __init__(self, title, description, creation_timestamp, ending_timestamp ,flag=False):
+        self.title = title
+        self.description = description
+        self.isComplete = flag
+        self.Creation_Timestamp = creation_timestamp
+        self.Ending_Timestamp = ending_timestamp
+
+
+    def __repr__(self):
+        return f"Task('{self.title}', '{self.description}', '{self.deadline}', '{self.flag}')"
+
+
+
+@app.route('/')
+def home_page():
+    todo_list = Task.query.all()
+    return render_template("base.html" ,todo_list = todo_list)
+
+@app.route("/add", methods =["POST"])
+def add():
+    title = request.form.get("Title")
+    description = request.form.get("Description")
+    if not title:
+        flash("Title cannot be empty", "error")
+        return redirect(url_for("home_page"))
+    
+    current_time = dt.datetime.now()
+    new_task = Task(title,description,current_time,current_time + dt.timedelta(days=1) )
+    db.session.add(new_task)
+    db.session.commit()
+    flash("Task Added Successfully!")
+    return redirect(url_for("home_page"))
+
+@app.route("/update/<int:task_id>")
+def update(task_id):
+    update_task = Task.query.filter_by(id=task_id).first()
+    update_task.isComplete =  not update_task.isComplete   
+    db.session.commit()
+    return redirect(url_for("home_page"))
+
+
+@app.route("/delete/<int:task_id>")
+def delete(task_id):
+    delete_task = Task.query.filter_by(id = task_id).first()
+    db.session.delete(delete_task)
+    db.session.commit()
+    return redirect(url_for("home_page"))
+
+
+@app.route("/generate_description")
+def generate_description():
+    title = request.args.get('title')
+    print("Title is ===> " , title)
+    if not title:
+        return jsonify({"description" : "title is Empty"}), 400
+    # model = MistralForCausalLM.from_pretrained("mistralai/Mistral-7B-v0.1")
+    # tokenizer = AutoTokenizer.from_pretrained("mistralai/Mistral-7B-v0.1")
+    generator = pipeline("text-generation", model="gpt2")
+    prompt = f"Write a task description for the Task: '{title}'"
+    ai_response = generator(prompt, num_return_sequences = 1, max_length=90)
+    # inputs = tokenizer(prompt,return_tensors="pt")
+    # print(prompt)
+    print(ai_response)
+    # generate_ids = model.generate(inputs.input_ids, max_length=30)
+    # response = tokenizer.batch_decode(generate_ids, skip_special_tokens=True, clean_up_tokenization_spaces=False)[0]
+    # print(response)
+    return jsonify({"description" :ai_response[0]['generated_text']}) , 200
+
+
+if __name__ == "__main__":  
+    with app.app_context():
+        db.create_all() 
+    app.run(debug=True)
+    
+
